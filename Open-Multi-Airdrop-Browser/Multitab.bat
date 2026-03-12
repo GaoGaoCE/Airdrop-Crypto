b432663d147b61c71039448d9f120cac55192d9b
GaoGaoCE <Hai.Dao@ssit.com.vn>
2026-03-12 08:35:10 +0700

    Upload script chạy tab daily task 1 lúc để tránh quên ^^

---
+++ Open-Multi-Airdrop-Browser/Multitab.bat
@@ -0,0 +1,10 @@
+@echo off
+start chrome https://cloud.google.com/application/web3/faucet/ethereum/sepolia
+start chrome https://app.prismax.ai/start
+start chrome https://gm.inkonchain.com/
+start chrome https://hub.playai.network/missions
+start chrome https://waitlist.kindredlabs.ai/dashboard
+start chrome https://rewards.dtelecom.org/reward?referral_code=GGCRYPTO
+start chrome https://app.decibel.trade/trade
+start chrome https://dream.degen.tips/dashboard
+start chrome https://portal.stormrae.ai?ref=gaogaocrypto
---
+++ Open-Multi-Airdrop-Browser/README.md
@@ -0,0 +1,10 @@
+## Giới Thiệu
+Đây là thư mục chứa script dành riêng cho việc mở cùng lúc nhiều trang web làm airdrop 
+Có thể đối với bạn nó không hay, nhưng nó tiện với mình ^^
+
+# Tính Năng
+Chỉ có 1 tính năng duy nhất là mở 1 lần nhiều tab,Để làm các task daily & tránh bị quên
+
+## Sử Dụng Như Thế Nào ?
+Chỉ cần click vào file .bat & chạy
+Nếu muốn thêm,sửa hay xoá website nào thì edit file .bat bằng notepad  và chỉnh sửa lại trang web bạn muốn là được 
